05092f480980e00bbec6cdb2746714a22d8301ee
SMTOWNMUSIC <gpx5000@psu.edu>
2017-03-31 12:40:52 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 
      public static void main(String []args){
         System.out.println("Hello World my name is Steven Miller");
+        System.out.println("I like to eat Organic and Gluten Free Foods that are low in calories and low in cholesterol");
      }
 }
